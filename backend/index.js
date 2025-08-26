6e3a8d5980eeb02a0fa8716bf063b2a787655388
ferLaika <fedesalvideal@outlook.com>
2025-08-26 15:42:21 -0500

    CRUD working

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,43 @@ app.post('/task', (req, res) => {
   nextId++;
 });
 
+//delete a task - DELETE:
+app.delete('/task/:id', (req, res) => {
+  let found = false;
+  let {id} = req.params;
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].id === Number(id)){ //find task
+      found = true;
+      tasks.splice(i, 1) //delete i
+      break;
+    }
+  }
+  if(found)
+    res.status(200).send('task deleted');
+  else
+    res.status(404).send('ERROR task not found');
+});
 
-//delete a task
+//edit a task - PUT:
+app.put('/task/:id', (req, res) => {
+let found = false;
+  let {id} = req.params;
+  let {content, done} = req.body;
+  for(let i = 0; i < tasks.length; i++){
+    if(tasks[i].id === Number(id)){ //find task
+      found = true;
+      if(content != undefined){
+        tasks[i].content = content;
+      }
+      if(done != undefined){
+        tasks[i].done = done;
+      }
+      break;
+    }
+  }
+  if(found)
+    res.status(200).send('task modified');
+  else
+    res.status(404).send('ERROR task not found');
+});
 
