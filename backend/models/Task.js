2e1faa0769f8ee30554424a9f250c6d9f03e7bd8
ferLaika <fedesalvideal@outlook.com>
2025-08-27 17:55:40 -0500

    setup React

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -5,6 +5,7 @@ const taskSchema = new mongoose.Schema({
     content: {type: String, required: true},
     done: {type: Boolean, default: false},
     date: {type: Date, default: Date.now},
+    deadline: {type: Date},
 });
 
 module.exports = mongoose.model('Task', taskSchema);
