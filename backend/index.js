d7c468e70c20b8200fefc3d4d8ac25de83f98b85
ferLaika <fedesalvideal@outlook.com>
2025-08-27 14:50:59 -0500

    change port

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,30 @@
 const express = require('express'); //import library to use express
 const dotenv = require('dotenv'); //import db
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 //DATABASE CONNECTION ---------------------------
 dotenv.config()
-connectDB();
 
 const app = express(); //create the server instance
-const PORT = process.env.PORT || 3000;
-
-
+app.use(cors()); // allows all origins
+app.use(express.json())
 
 //add a route:
 app.get('/', (req, res) => {
   res.send('hello world!'); //test a route (homepage)
 });
 
-app.listen(PORT, () => {
-  console.log(`our server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 4000;
+console.log('Starting server...');
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.error('DB connection failed', err);
 });
 
-//apply middleware:
-app.use(express.json())
 
 //import Task model:
 const Task = require('./models/Task');
@@ -55,14 +58,13 @@ app.post('/task', async (req, res) => {
 });
 
 //delete a task - DELETE:
-app.delete('/task:id', async (req, res) => {
+app.delete('/task/:id', async (req, res) => {
   try{
     const deletedTask = await Task.findByIdAndDelete(req.params.id);
     if(!deletedTask){
       return res.status(404).send('not found');
     }
     res.send('task deleted');
-    res.status(400).send('Invalid ID');
   }
   catch(err){
     res.status(500).json({message: 'failed to create task', error: err.message});
