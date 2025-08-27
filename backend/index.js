70346624c7e2ae8b1247e86ae9692d9ba22f435f
ferLaika <fedesalvideal@outlook.com>
2025-08-27 13:20:20 -0500

    send response to client

--- backend/index.js
+++ backend/index.js
@@ -29,12 +29,13 @@ const Task = require('./models/Task');
 
 //GET request - return tasks array, from the backend to the frontend
 
+
 app.get('/task', async (req, res) => {
   try {
     const tasks = await Task.find();
     res.status(200).json(tasks);
   } catch (err) {
-    res.status(500).json({message: 'failed to fetch task', error: err.message});
+    res.status(500).json({message: 'failed to fetch task', error: err.message})
   }
 });
 
