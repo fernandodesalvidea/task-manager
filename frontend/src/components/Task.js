70d4a83a80e1f7a2abe099edb619a084a6e5a898
ferLaika <fedesalvideal@outlook.com>
2025-08-28 12:28:16 -0500

    add buttons

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,9 +1,18 @@
 import './Task.css';
+import { MdModeEdit } from "react-icons/md";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { FaTrashAlt } from "react-icons/fa";
+
 
 function Task({content}){
     return(
     <div className='task'>
         {content}
+        <div className='right'>
+            <button type='button' id='done'><MdModeEdit /></button>
+            <button type='button' id='edit'><IoIosCheckmarkCircle /></button>
+            <button type='button' id='delete'><FaTrashAlt /></button>
+        </div>
     </div>
     );
 }
