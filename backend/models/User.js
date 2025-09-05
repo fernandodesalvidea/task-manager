12d7e8270f977b3013d678d21a4d839f750ca2c3
ferLaika <fedesalvideal@outlook.com>
2025-09-05 11:28:52 -0500

    make user schema and add password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+bcrypt = require('bcrypt'),
+SALT_WORK_FACTOR = 10;
 
 //user schema
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: true},
+    createdAt: {type: Date, default: Date.now()}
+});
 
-module.exports = mongoose.model('User', userSchema);
+//runs BEFORE saving a user to the database. 'this' is the current user document
+UserSchema.pre('save', function(next){
+    let user = this;//get access to the user model
+
+    if(!user.isModified('password')) return next(); //only hash password if it has been modified
+
+    //generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if (err) return next(err);
+
+        //hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err) return next(err);
+             // override the cleartext password with the hashed one
+             user.password = hash;
+             next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
